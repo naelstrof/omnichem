78208877dbbece822a7a5144516add5aae036135
naelstrof <naelstrof@gmail.com>
2016-03-14 19:06:04 -0700

    Added depgraph generation

---
+++ public/scripts/get.js
@@ -0,0 +1,44 @@
+var redraw, g, renderer;
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF'.split('');
+    var color = '#';
+    for (var i = 0; i < 6; i++ ) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+$(document).ready(function() {
+    g = new Graph();
+    var render = function(r, node) {
+        /* the default node drawing */
+        var color = node.color;
+        var ellipse = r.ellipse(0, 0, 30, 20).attr({fill: color, stroke: color, "stroke-width": 2});
+        /* set DOM node ID */
+        ellipse.node.id = node.label || node.id;
+        shape = r.set().
+            push(ellipse).
+            push(r.text(0, 30, node.label || node.id));
+        return shape;
+    }
+    $('edge').each( function( index ) {
+        var strings = $(this).text().split(",");
+        g.addNode( strings[1], { color : getRandomColor(), render : render } );
+        g.addNode( strings[0], { color : getRandomColor(), render : render } );
+        g.addEdge( strings[1], strings[0], { directed: true } );
+    } );
+    for(e in g.edges) {
+        g.edges[e].style.stroke = g.edges[e].source.color;
+        g.edges[e].style.fill = g.edges[e].source.color;
+    }
+    var layouter = new Graph.Layout.Spring(g);
+    renderer = new Graph.Renderer.Raphael('canvas', g, $("#content").width(), $("#content").height() );
+    redraw = function() {
+        layouter.layout();
+        renderer.width = $("#content").width();
+        renderer.height = $("#content").height() | 400;
+        renderer.draw();
+    };
+    $(window).resize( redraw );
+} );
--- source/chemAPI.d
+++ source/chemAPI.d
@@ -12,6 +12,75 @@ class Reagent {
     int[string] required_reagents;
     int required_temperature;
     int result_amount;
+    /*private int getDepth( Reagent[string] reagents, Reagent cur, int curDepth = 0 ) {
+        int depth = curDepth;
+        int deepestDepth = depth;
+        foreach( r ; cur.required_reagents.keys ) {
+            int temp = getDepth( reagents, reagents[r], depth+1 );
+            if ( temp > deepestDepth ) {
+                deepestDepth = temp;
+            }
+        }
+        return deepestDepth;
+    }
+    private int getWidth( Reagent[string] reagents, Reagent cur ) {
+        int depth = getDepth( reagents, cur );
+        int greatestWidth = 0;
+        for( int i = 0 ; i < depth; i++ ) {
+            greatestWidth = max( cast(int)getLevel( reagents, cur, depth ).length, greatestWidth );
+        }
+        return greatestWidth;
+    }
+    private string[] getLevel( Reagent[string] reagents, Reagent top, int level ) {
+        string[] cur = [ top.id ];
+        int curLevel = 0;
+        while( curLevel != level ) {
+            string[] temp;
+            foreach( str ; cur ) {
+                temp ~= reagents[str].required_reagents.keys;
+            }
+            cur = temp;
+            curLevel++;
+        }
+        return cur;
+    }
+    string generateColumns( Reagent[string] reagents, Reagent cur, int fillWidth ) {
+        string html = "";
+        if ( cur.required_reagents.length <= 0 ) {
+            return html;
+        }
+        int split = cast(int)fillWidth / cast(int)cur.required_reagents.length;
+        foreach( r ; cur.required_reagents.keys ) {
+            html ~= `<td colspan="` ~ split.to!string ~ `">` ~ reagents[r].name ~ `</td>`;
+        }
+        int splitrem = cast(int)fillWidth % cast(int)cur.required_reagents.length;
+        if ( splitrem > 0 ) {
+            html ~= `<td colspan="` ~ splitrem.to!string ~ `"></td>`;
+        }
+        return html;
+    }
+    string generateRow( Reagent[string] reagents, string[] list, int width ) {
+        string html = `<tr>`;
+        foreach( str ; list ) {
+            html ~= generateColumns( reagents, reagents[str], cast(int)width/cast(int)list.length );
+        }
+        int splitrem = cast(int)width % cast(int)list.length;
+        if ( splitrem > 0 ) {
+            html ~= `<td colspan="` ~ splitrem.to!string ~ `"></td>`;
+        }
+        html ~= `</tr>`;
+        return html;
+    }
+    string generateDependencyGraph( Reagent[string] reagents, Reagent top ) {
+		int depth = getDepth( reagents, top );
+		int width = getWidth( reagents, top );
+		string html = `<table class="table table-striped table-bordered"><tbody>`;
+		html ~= `<tr><td colspan="`~width.to!string~`">` ~ top.name ~ `</td></tr>`;
+        for( int i = 0 ; i < depth; i++ ) {
+            html ~= generateRow( reagents, getLevel( reagents, top, i ), width );
+        }
+        return html;
+    } */
 }
 
 interface IChemAPI {
@@ -24,8 +93,21 @@ class ChemAPI : IChemAPI {
     public Reagent[string] reagents;
     // For sorted
     public Reagent[] sorted_reagents;
+    private string generateEdges( Reagent top ) {
+        string html;
+        foreach( str ; top.required_reagents.keys ) {
+            if( !reagents.keys.canFind(str) ) {
+                html ~= `<edge hidden>"`~ top.name ~ `","` ~ str ~ `"</edge>`;
+            } else {
+                html ~= `<edge hidden>"`~ top.name ~ `","` ~ reagents[str].name ~ `"</edge>`;
+            }
+            html ~= generateEdges( reagents[str] );
+        }
+        return html;
+    }
     void getReagent( string id ) {
-        render!("get.dt", id, reagents);
+        string html = generateEdges( reagents[id] );
+        render!("get.dt", html );
     }
     void index() {
         render!("index.dt", sorted_reagents);
@@ -37,9 +119,13 @@ class ChemAPI : IChemAPI {
             Reagent r = new Reagent();
             r.name = j["name"].str;
             r.id = j["id"].str;
-            JSONValue[string] array = j["required_reagents"].object;
-            foreach( str ; array.byKey ) {
-                r.required_reagents[str] = array[str].integer.to!int;
+            if ( auto test = "required_reagents" in j ) {
+                if ( test != null ) {
+                    const JSONValue[string] array = test.object;
+                    foreach( str ; array.byKey ) {
+                        r.required_reagents[str] = array[str].integer.to!int;
+                    }
+                }
             }
             if ( auto test = "required_temperature" in j ) {
                 if ( test == null ) {
